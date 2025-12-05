12e8a043fe8ad13c40cb230534f37e2448ba0cc2
LanQian <2723104869@qq.com>
2025-12-05 14:42:43 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,4 +7,28 @@ $.ajaxPrefilter(function (options) {
     options.url = 'http://ajax.fronted.itheima.net' + options.url
     console.log(options.url)
 
+    //统一为有权限的接口，设置headers请求头
+    // 索引不包含/my/且索引不是-1的
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+
+        }
+    }
+
+    // 全局统一挂载complete回调函数
+    options.complete = function (res) {
+        // console.log('执行了complete回调：')
+        // console.log(res)
+        //在complete回调函数中可以使用res.responseJSON 拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.强制清空token
+            localStorage.removeItem('token')
+            // 2.强制跳转到登录页面
+            location.href = '/8.前后端交互/12.大事件后台管理系统/login.html'
+        }
+
+    }
+
+
 })
